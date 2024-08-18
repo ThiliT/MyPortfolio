9a13a75d61c460807a3250a7533f921af1ad6c40
Tharushika M.T. <thilinitharushika.21@cse.mrt.ac.lk>
2024-08-18 18:57:50 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,18 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import {BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename="/MyPortfolio">
     <App />
   </BrowserRouter>
 );
-
-/*
-ReactDOM.render(
-  <BrowserRouter>
-    <App/>
-  </BrowserRouter>
-);*/
